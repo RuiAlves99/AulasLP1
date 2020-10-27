6ec2def2746aa56bb807f1dc97204cf4d3b0131a
Rui Alves <Aulas@MacBook-Pro-de-Rui.local>
2020-10-27 22:37:00 +0000

    Added aula1

---
+++ aula1/ex1/aula.c
@@ -0,0 +1,22 @@
+/**
+ * @file aula.c
+ * @author Rui Alves (a15505@alunos.ipca.pt)
+ * @brief aula
+ * @version 0.1
+ * @date 2020-10-27
+ * 
+ * @copyright Copyright (c) 2020
+ * 
+ */
+#include <stdio.h>
+
+/**
+ * @brief Programa principal
+ * 
+ * @return int 
+ */
+int main() 
+{
+    int x = 1;
+    printf("%d\n", x);
+}
